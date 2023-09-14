ubports-clover/android_kernel_xiaomi_clover

drivers/media/platform/msm/camera_v2/sensor: Import Xiaomi changes

drivers/media/platform/msm/camera_v2/sensor/msm_sensor_init.c
@@ -42,15 +42,22 @@ static const struct v4l2_subdev_internal_ops msm_sensor_init_internal_ops;
 static int msm_sensor_wait_for_probe_done(struct msm_sensor_init_t *s_init)
 {
 	int rc;
+#ifndef CONFIG_XIAOMI_CLOVER
 	int tm = 20000;
+#endif
 	if (s_init->module_init_status == 1) {
 		CDBG("msm_cam_get_module_init_status -2\n");
 		return 0;
 	}
+#ifdef CONFIG_XIAOMI_CLOVER
+	wait_event(s_init->state_wait,
+		(s_init->module_init_status == 1));
+#else
 	rc = wait_event_timeout(s_init->state_wait,
 		(s_init->module_init_status == 1), msecs_to_jiffies(tm));
 	if (rc == 0)
 		pr_err("%s:%d wait timeout\n", __func__, __LINE__);
+#endif
 
 	return rc;
 }

rongi5/kernel_xiaomi_sdm660

(cherry picked from commit c86c102f49456db82eae5a2a63e0043507cbf66a)
Signed-off-by: pix106 <sbordenave@gmail.com>

drivers/media/platform/msm/camera_v2/sensor/msm_sensor_init.c
@@ -52,16 +52,23 @@ static int msm_sensor_wait_for_probe_done(struct msm_sensor_init_t *s_init)
 		(s_init->module_init_status == 1));
 #else
 	int rc;
+#ifndef CONFIG_MACH_XIAOMI_CLOVER
 	int tm = 20000;
+#endif
 
 	if (s_init->module_init_status == 1) {
 		CDBG("msm_cam_get_module_init_status -2\n");
 		return 0;
 	}
+#ifdef CONFIG_MACH_XIAOMI_CLOVER
+	wait_event(s_init->state_wait,
+		(s_init->module_init_status == 1));
+#else
 	rc = wait_event_timeout(s_init->state_wait,
 		(s_init->module_init_status == 1), msecs_to_jiffies(tm));
 	if (rc == 0)
 		pr_err("%s:%d wait timeout\n", __func__, __LINE__);
+#endif
 #endif
 
 	return rc;